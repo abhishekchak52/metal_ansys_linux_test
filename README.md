e730ae9d824c5a167b1acfe24e4e88ea0569c3e8
Abhishek Chakraborty <abhishekchak52@gmail.com>
2026-01-13 14:38:58 -0800

    Added LOM functionality test script

--- README.md
+++ README.md
@@ -7,6 +7,35 @@ git clone --recurse-submodules https://www.github.com/abhishekchak52/quantum-met
 ```
 
 We use uv to manage dependencies for this project. First install `uv` on your system ([installation instructions here](https://docs.astral.sh/uv/getting-started/installation/)). 
+
+## Test Scripts
+
+There are some test scripts to check functionality (more to be added later): 
+
+- [lom_test.py](scripts/lom_test.py)
+
+Run them using the following command: 
+
+```sh
+uv run ./scripts/lom_test.py
+```
+
+Currently, running the script will bring up the Ansys EDT GUI. You can see the design being drawn inside of Ansys. After that, the Q3D simulation will run for a number of iterations. Finally the capacitance matrix should be printed out as a pandas DataFrame and the Ansys GUI will exit.  
+
+```
+                          bus1_connector_pad_Q1  bus2_connector_pad_Q1  ground_main_plane  pad_bot_Q1  pad_top_Q1  readout_connector_pad_Q1        
+bus1_connector_pad_Q1                 51.108579              -0.420905         -34.350276   -1.560883  -13.640273                 -0.204364        
+bus2_connector_pad_Q1                 -0.420905              55.440790         -36.740701  -14.419409   -1.849197                 -1.031396        
+ground_main_plane                    -34.350276             -36.740701         239.516767  -31.539787  -38.285971                -37.526670        
+pad_bot_Q1                            -1.560883             -14.419409         -31.539787  100.177557  -30.988633                -19.467099        
+pad_top_Q1                           -13.640273              -1.849197         -38.285971  -30.988633   89.664766                 -2.245261        
+readout_connector_pad_Q1              -0.204364              -1.031396         -37.526670  -19.467099   -2.245261                 61.534274 
+```
+Your result may differ from the one shown above, but the general structure and row/column names should be the same. 
+
+
+## Test notebooks
+
 Run the following command in the root of the repository. This will open the tutorial notebooks from the qiskit-metal repo: 
 
 ```
---
+++ scripts/lom_test.py
@@ -0,0 +1,90 @@
+from qiskit_metal import designs
+from qiskit_metal import Dict
+
+from qiskit_metal.qlibrary.qubits.transmon_pocket import TransmonPocket
+from qiskit_metal.qlibrary.tlines.meandered import RouteMeander
+
+from qiskit_metal.analyses.quantization import LOManalysis
+
+design = designs.DesignPlanar()
+
+design.variables['cpw_width'] = '15 um'
+design.variables['cpw_gap'] = '9 um'
+
+# Enable overwrite, so that we can re-un this cell multiple times without incurring object name collisions
+## Custom options for all the transmons
+options = dict(
+    # Some options we want to modify from the defaults
+    # (see below for defaults)
+    pad_width = '425 um', 
+    pocket_height = '650um',
+    # Adding 4 connectors (see below for defaults)
+    connection_pads=dict(
+        readout = dict(loc_W=+1,loc_H=-1, pad_width='200um'),
+        bus1 = dict(loc_W=-1,loc_H=+1, pad_height='30um'),
+        bus2 = dict(loc_W=-1,loc_H=-1, pad_height='50um')
+    )
+)
+
+## Create 4 transmons
+
+q1 = TransmonPocket(design, 'Q1', options = dict(
+    pos_x='+2.42251mm', pos_y='+0.0mm', **options))
+q2 = TransmonPocket(design, 'Q2', options = dict(
+    pos_x='+0.0mm', pos_y='-0.95mm', orientation = '270', **options))
+q3 = TransmonPocket(design, 'Q3', options = dict(
+    pos_x='-2.42251mm', pos_y='+0.0mm', orientation = '180', **options))
+q4 = TransmonPocket(design, 'Q4', options = dict(
+    pos_x='+0.0mm', pos_y='+0.95mm', orientation = '90', **options))
+
+RouteMeander.get_template_options(design)
+
+options = Dict(
+        lead=Dict(
+            start_straight='0.2mm',
+            end_straight='0.2mm'),
+        trace_gap='9um',
+        trace_width='15um')
+
+def connect(component_name: str, component1: str, pin1: str, component2: str, pin2: str,
+            length: str, asymmetry='0 um', flip=False, fillet='90um'):
+    """Connect two pins with a CPW."""
+    myoptions = Dict(
+        fillet=fillet,
+        hfss_wire_bonds = True,
+        pin_inputs=Dict(
+            start_pin=Dict(
+                component=component1,
+                pin=pin1),
+            end_pin=Dict(
+                component=component2,
+                pin=pin2)),
+        total_length=length)
+    myoptions.update(options)
+    myoptions.meander.asymmetry = asymmetry
+    myoptions.meander.lead_direction_inverted = 'true' if flip else 'false'
+    return RouteMeander(design, component_name, myoptions)
+
+asym = 140
+cpw1 = connect('cpw1', 'Q1', 'bus2', 'Q2', 'bus1', '6.0 mm', f'+{asym}um')
+cpw2 = connect('cpw2', 'Q3', 'bus1', 'Q2', 'bus2', '6.1 mm', f'-{asym}um', flip=True)
+cpw3 = connect('cpw3', 'Q3', 'bus2', 'Q4', 'bus1', '6.0 mm', f'+{asym}um')
+cpw4 = connect('cpw4', 'Q1', 'bus1', 'Q4', 'bus2', '6.1 mm', f'-{asym}um', flip=True)
+
+c1 = LOManalysis(design, "q3d")
+# example: update single setting
+c1.sim.setup.max_passes = 15
+c1.sim.setup.min_passes = 1
+c1.sim.setup.min_converged_passes = 3
+# example: update multiple settings
+c1.sim.setup_update(solution_order = 'Medium', auto_increase_solution_order = 'False')
+
+
+
+try:   
+    c1.sim.run(components=['Q1'], open_terminations=[('Q1', 'readout'), ('Q1', 'bus1'), ('Q1', 'bus2')])
+    print(c1.sim.capacitance_matrix)
+except Exception:
+    pass
+finally:
+    c1.sim.close()
